716f4b73d9696c5d2e258a66c6acc9e27e38ca83
aj132608 <32584157+aj132608@users.noreply.github.com>
2020-02-02 23:32:04 -0600

    Renamed resource_request to resourcerequest and added submissionservice along with a default config.json for testing.

---
+++ submissionservice/submission_service.py
@@ -0,0 +1,75 @@
+import calendar
+import time
+
+# put imports from other python classes here
+
+
+class SubmissionService:
+
+    def __init__(self, config_path):
+        self.config_path = config_path
+        self.config = None
+
+    def connect_to_queue(self, queue_name=None):
+        pass
+
+    def submit_task(self, payload_path, queue_name, task_id=None):
+        """
+
+        This function will start a task, add it to the queue of tasks, and store the reference information
+        in a cloud or local environment.
+
+        :param payload_path:
+        :param queue_name:
+        :param task_id:
+        :return:
+        """
+
+        if task_id is None:
+            task_id = SubmissionService.generate_task_id()
+
+        # Add the task ID to the Queue
+
+        # Store the payload path with corresponding task ID in a location
+
+    def submit_task_with_data(self, payload_path, data_path, queue_name, task_id=None):
+        """
+
+                This function will start a task, add it to the queue of tasks, and store the reference information
+                in a cloud or local environment.
+
+                :param payload_path:
+                :param queue_name:
+                :param task_id:
+                :return:
+                """
+
+        if task_id is None:
+            task_id = SubmissionService.generate_task_id()
+
+        # Add the task ID to the Queue
+
+        # Store the payload path with corresponding task ID in a location
+
+    @staticmethod
+    def generate_task_id(base_word=None):
+        """
+
+        This function will generate a task id with a given or default base word
+
+
+        :param base_word: an optional argument if you wanted a specific word for your task. It defaults to 'task'
+        :return: task_id: unique string generated for each task submitted
+        """
+
+        # check to see if you should add the default base word
+        if base_word is None:
+            base_word = "task"
+
+        # generate a number
+        id_number = calendar.timegm(time.gmtime())
+
+        # assemble the task id
+        task_id = f"{base_word}_{id_number}"
+
+        return task_id
