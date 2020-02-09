aa51d19158be9c0f068d1d36b23564907218d182
aj132608 <32584157+aj132608@users.noreply.github.com>
2020-02-08 18:54:01 -0600

    Added the boto3 resources to EC2_instance_creation

--- services/queuingservices/rabbitmq/practice_queue.py
+++ services/queuingservices/rabbitmq/practice_queue.py
@@ -1,4 +1,19 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+
+class PracticeQueue:
+    def __init__(self):
+        self.connection = None
+        self.channel = None
+
+    def establish_connection(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+
+    def tutorial_test_1(self):
+        self.establish_connection()
+
+
+if __name__ == "__main__":
+    queue_obj = PracticeQueue()
+    queue_obj.tutorial_test_1()
