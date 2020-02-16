faf977215e45762de36d6e07aaf6b682d92b9f44
Tegasaur <samuel.o.okei@ttu.edu>
2020-02-16 00:04:06 -0600

    workers_done

--- tests/gcloud_test.py
+++
@@ -1,6 +0,0 @@
-from storage.gcloud_api import GCloudApi
-
-
-app = GCloudApi("storage/my-project1-254915-24b914b345ec.json", "new_bucket",
-                "tests/funny.PNG", "funny")
-app.persist()
