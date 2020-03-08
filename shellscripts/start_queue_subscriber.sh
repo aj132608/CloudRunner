0e87544887632feb3d43f4989f26d3dcd91ad250
Mohak Kant <mohak.kant1amg@gmail.com>
2020-03-07 21:00:02 -0600

    Fixed Some stuff

--- shellscripts/start_queue_subscriber.sh
+++ shellscripts/start_queue_subscriber.sh
@@ -3,4 +3,5 @@ queue_config_path="/.mineai/configs/queue_config.json"
 while [ ! -f $queue_config_path ]; do sleep 1; done
 
 echo "Starting Queue Server"
-python3 -m worker.initialize_worker
+sudo touch ./mineai/subscriber_logs.txt
+sudo python3 -m worker.initialize_worker > ./mineai/subscriber_logs.txt
